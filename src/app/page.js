4f6752f45eb337a8b450824ae139fb960b6c3815
stefanRachev <starfystef@gmail.com>
2024-11-26 20:54:27 +0200

    add actions ,index.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import LoginForm from "@/components/LoginForm";
+
+LoginForm;
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center m-4">
       <h1 className="text-3xl my-3">Hey, time to Sign In</h1>
+      <LoginForm />
     </div>
   );
 }
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,9 +1,14 @@
-
-
 const LoginForm = () => {
   return (
-    <div>LoginForm</div>
-  )
-}
+    <form action="">
+      <button type="submit" name="action" value="google" className="bg-pink-400 text-white p-1 rounded-md m-1 text-lg">
+        Sign In With Google
+      </button>
+      <button type="submit" name="action" value="github" className="bg-black text-white p-1 rounded-md m-1 text-lg">
+        Sign In With GitHub
+      </button>
+    </form>
+  );
+};
 
-export default LoginForm
+export default LoginForm;
