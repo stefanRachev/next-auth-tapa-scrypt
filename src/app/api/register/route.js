06e1925d1236bb705ed398c2d88a66d55228c1f2
stefanRachev <starfystef@gmail.com>
2024-11-28 11:48:30 +0200

    install db mongoose

---
+++ src/app/api/register/route.js
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+
+export const POST = async (request) => {
+  const { name, email, password } = await request.json();
+
+  console.log(name, email, password);
+
+  return new NextResponse("User has been created", {
+    status: 201,
+  });
+};
--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -1,11 +1,48 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import SocialLogin from "./SocialLogin";
 
 
-
 const RegistrationForm = () => {
+
+  const router = useRouter();
+
+  async function handlerSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const formData = new FormData(e.currentTarget);
+      const name = formData.get("name");
+      const email = formData.get("email");
+      const password = formData.get("password");
+
+      const response = await fetch("/api/register", {
+        method:"POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+        }),
+      });
+
+      response.status === 201 && router.push("/")
+
+
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
   return (
     <>
-      <form className="my-5 flex flex-col items-center border p-3 border-gray-200 rounded-md">
+      <form
+        onSubmit={handlerSubmit}
+        className="my-5 flex flex-col items-center border p-3 border-gray-200 rounded-md"
+      >
         <div className="my-2 w-full flex flex-col items-start">
           <label htmlFor="name" className="mb-1 text-sm font-medium">
             Name
